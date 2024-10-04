52c5c498c43e8f575404363126c474b2fe501e75
Jack Weekes <git@jackweekes.com>
2024-10-04 15:44:47 +0100

    Add files via upload
    Added experimental support for Windows.

--- featureSetLocaleSwitcher.jsx
+++ featureSetLocaleSwitcher.jsx
@@ -1,14 +1,14 @@
 //
 // INDESIGN FEATURE SET LOCALE SWITCHER by JACK WEEKES
 // 
-// VERSION 1.6
+// VERSION 2.0
 //
 // Script UI built using: ScriptUI Dialog Builder at: https://scriptui.joonas.me/
 // Based on the findings of Dr Ken Lunde at: https://medium.com/@ken.lunde/adobe-indesign-tips-japanese-cjk-functionality-english-ui-redux-539528e295c6
 //
-const appVersion = "1.6";
+const appVersion = "2.0";
 
-if ($.os.slice(0,3) == "Mac") { // Only runs on macOS
+if ($.os.slice(0,3) == "Mac") { // macOS
     var PREF_STATIC = "/Presets/applicationpreferences/indesign/applicationpreference.plist" // Location of the applicationpreference.plist file, not including the path to the application
     var appPreferenceFull = Folder(app.extractLabel("appPreferenceFolder_ST54RETY54")); // Gets the full applicationpreference.plist as saved in InDesign (if run previously).
     var userAbort = false;
@@ -27,17 +27,59 @@ if ($.os.slice(0,3) == "Mac") { // Only runs on macOS
         var configXML = appPreferenceFull.read();
         appPreferenceFull.close();
     }
+} else if ($.os.slice(0,3) == "Win") { // Windows
+    const mySaveFolder = $.getenv("APPDATA") + "\\Adobe\\InDesign\\Locale Script ST54RETY54\\"; // Folder path to save reg files in
+    const myAppVersion = app.version.substring(0,2); // gets fist two digits of version number to use in filename and reg file
+    const myFileNameRoman = myAppVersion + "_roman.reg";
+    const myFileNameRtL = myAppVersion + "_rtl.reg";
+    const myFileNameCJK = myAppVersion + "_cjk.reg";
+    const myLocaleNumberRoman = 0;
+    const myLocaleNumberRtL = 0;
+    const myLocaleNumberCJK = 0;
+    const myRegData = "Windows Registry Editor Version 5.00\n\n[HKEY_LOCAL_MACHINE\\SOFTWARE\\Adobe\\InDesign\\" + myAppVersion + ".0]\n\"Feature Set Locale Setting\"=dword:";
+    const myRegDataRoman = myRegData + "00000100"; // 256
+    const myRegDataRtL = myRegData + "00000103";   // 259
+    const myRegDataCJK = myRegData + "00000101";   // 257
+
+    const myRegFileRoman = File(mySaveFolder + myFileNameRoman);
+    const myRegFileRtL = File(mySaveFolder + myFileNameRtL);
+    const myRegFileCJK = File(mySaveFolder + myFileNameCJK);
+
+
+    if (!mySaveFolder.exists) {
+        Folder(mySaveFolder).create()
+    }
+    if (!myRegFileRoman.exists) {
+        myRegFileRoman.open('w', undefined, undefined);
+        myRegFileRoman.write(myRegDataRoman);
+        myRegFileRoman.close();
+    }
+    if (!myRegFileRtL.exists) {
+        myRegFileRtL.open('w', undefined, undefined);
+        myRegFileRtL.write(myRegDataRtL);
+        myRegFileRtL.close();
+    }
+    if (!myRegFileCJK.exists) {
+        myRegFileCJK.open('w', undefined, undefined);
+        myRegFileCJK.write(myRegDataCJK);
+        myRegFileCJK.close();
+    }
+
+}
+
 const permissionIssue = "Unable to edit the following file:\n\n" + appPreferenceFull + "\n\nEnsure you have write access and try again.";
+const permissionIssueWin = "File didn't run, ensure you have the correct permissions (eg admin rights) and try again."
 
 var originalConfigXML = configXML; //Used to see if the the plist was written to
-
+var XML_Val;
+if ($.os.slice(0,3) == "Mac") { // macOS
     if (!userAbort) {
         if (configXML == "") {
             alert("Error 001\n\n" + permissionIssue); //Please set permissions 
         } else {
             if (app.documents.length == 0) {
                 configXML = new XML(configXML);
-                var XML_Val;
+                XML_Val;
                 if (configXML.dict.string > 0) {
                     XML_Val = configXML.dict.string;
                 } else if (configXML.dict.integer > 0) {
@@ -49,7 +91,28 @@ var originalConfigXML = configXML; //Used to see if the the plist was written to
             }   
         }
     }
-    
+} else if ($.os.slice(0,3) == "Win") { // Windows
+    if (!userAbort) {
+        if (app.documents.length == 0) {
+            switch(app.featureSet) {
+                case 1247899758:
+                    XML_Val = 257;
+                    break;
+                case 1381265228:
+                    XML_Val = 259;
+                    break;
+                default:
+                   XML_Val = 256;
+} 
+            XML_Val
+            dialogShow();
+        } else {
+            alert("Please close all documents and try again.");
+        }   
+    }
+}
+
+
     function dialogShow() {
         // DIALOG
         // ======
@@ -135,27 +198,48 @@ var originalConfigXML = configXML; //Used to see if the the plist was written to
     }
     
     function saveLocale(ver) {
-        var plistBase = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n";
-        
-        if (configXML.dict.string > 0) {
-            configXML.dict.string = ver;
-        } else if (configXML.dict.integer > 0) {
-            configXML.dict.integer = ver;
-        } 
-        var newPlistConfigXML = plistBase + configXML.toXMLString();
+        if ($.os.slice(0,3) == "Mac") { // macOS
+            var plistBase = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n";
+            
+            if (configXML.dict.string > 0) {
+                configXML.dict.string = ver;
+            } else if (configXML.dict.integer > 0) {
+                configXML.dict.integer = ver;
+            } 
+            var newPlistConfigXML = plistBase + configXML.toXMLString();
 
-        appPreferenceFull.open("W");
-        appPreferenceFull.write(newPlistConfigXML);
-        appPreferenceFull.close();
+            appPreferenceFull.open("W");
+            appPreferenceFull.write(newPlistConfigXML);
+            appPreferenceFull.close();
 
-        if (newPlistConfigXML == originalConfigXML) {
-            app.quit();
-        } else {
-            alert("Error 002\n\n" + permissionIssue);
+            if (newPlistConfigXML == originalConfigXML) {
+                app.quit();
+            } else {
+                alert("Error 002\n\n" + permissionIssue);
+            }
+        } else if ($.os.slice(0,3) == "Win") { // Windows
+            var myErrorRegCan = true;
+            switch (ver) {
+                case 257: // JPN
+                    if(myRegFileCJK.execute()){
+                        myErrorRegCan = false;
+                    }
+                    break;
+                case 259: // RTL
+                    if(myRegFileRtL.execute()){
+                        myErrorRegCan = false;
+                    }
+                    break;
+                default:  // Roman
+                    if(myRegFileRoman.execute()){
+                        myErrorRegCan = false;
+                    }
+            }
+
+            if(myErrorRegCan) {
+                alert("Error 003\n\n" + permissionIssueWin);
+            } else {
+                app.quit();
+            }
         }
-
-        
     }
-} else {
-    alert("This script only works with macOS, Sorry!");
-}
